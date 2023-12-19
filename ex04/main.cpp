e4990148a11a4a5a1b2abdf65614e8220d2cc8ad
ndesprez <ndesprez@student.42nice.fr>
2023-12-19 14:51:29 +0100

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,18 +6,35 @@
 /*   By: ndesprez <ndesprez@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 12:26:15 by ndesprez          #+#    #+#             */
-/*   Updated: 2023/12/19 14:10:10 by ndesprez         ###   ########.fr       */
+/*   Updated: 2023/12/19 14:46:16 by ndesprez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-void	replace(std::string ogc, std::string filename, std::string ogss, std::string nss)
+int	replace(std::string ogc, std::string filename, std::string ogss, std::string nss)
 {
 	std::ofstream	nf;
+	int				x = -1;
 
-	nf.open(filename + ".replace")
+	nf.open((filename + ".replace").c_str());
+	if (!nf.is_open())
+		return (std::cerr << filename << ".replace creation error." << std::endl, 1);
+	for (int i = 0; i < (int)ogc.size(); i++)
+	{
+		if (x < i)
+			x = ogc.find(ogss, i);
+		if (x != i)
+			nf << ogc[i];
+		else
+		{
+			nf << nss;
+			i += ogss.size() - 1;
+		}
+	}
+	nf.close();
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -26,7 +43,7 @@ int	main(int argc, char **argv)
 	std::string		s;
 	char			c;
 	
-	if (argc != 4)
+	if (argc != 4 || argv[2][0] == '\0')
 		return (std::cerr << "Syntax error." << std::endl, 2);
 	ogf.open(argv[1]); //opens argv[1] file in ogf operator
 	if (!ogf.is_open())
@@ -34,6 +51,5 @@ int	main(int argc, char **argv)
 	while (!ogf.eof() && ogf >> std::noskipws >> c)
 		s += c;
 	ogf.close();
-	replace(s, argv[1], argv[2], argv[3]);
-	return (0);
+	return (replace(s, argv[1], argv[2], argv[3]));
 }
